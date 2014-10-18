d8d569922d52a8a62406509f6e8fcacc98da1ef3
sundhaug92 <martinsundhaug@gmail.com>
2014-10-18 19:32:45 +0200

    Take input from user

--- steamBPTest/Program.cs
+++ steamBPTest/Program.cs
@@ -13,10 +13,11 @@ namespace steamBPTest
     {
         static void Main(string[] args)
         {
-            SteamClient steam = new SteamClient("localhost");
+            Console.Write("Hostname=");
+            SteamClient steam = new SteamClient(Console.ReadLine());
 
-            Console.WriteLine("Authorizing");
-            steam.Authorize("Local C#");
+            Console.Write("App-name=");
+            steam.Authorize(Console.ReadLine());
 
             Console.WriteLine("SetVolume0");
             steam.Music.SetVolume(0);
@@ -28,10 +29,9 @@ namespace steamBPTest
             Thread.Sleep(500);
             Console.WriteLine("SetVolume1");
             steam.Music.SetVolume(1);
-            
 
-            Console.WriteLine("Starting game:220200");
-            steam.Games.Run(220200);
+            Console.Write("GameID=");
+            steam.Games.Run(int.Parse(Console.ReadLine()));
             /*
             var runGame = System.Text.Encoding.Default.GetString(
                     wc.UploadValues("https://localhost:27037/steam/games/220200/run",
